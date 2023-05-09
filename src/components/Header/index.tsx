a2ef4cf73eae469d402b9fe942bbab868287f7cf
Gabriel-Carvf <39717664+GabrielCarvf@users.noreply.github.com>
2023-05-08 22:22:38 -0300

    :lipstick: Estilização/Criação layout e header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from './styles'
+import logoIgnite from '../../assets/Logo.svg'
+import { Timer, Scroll } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={22} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
