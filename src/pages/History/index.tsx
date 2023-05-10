def84cc8da93187207013e8ef6a434339d8907d8
Gabriel-Carvf <39717664+GabrielCarvf@users.noreply.github.com>
2023-05-09 23:24:10 -0300

    :lipstick: Criando/Estilizando histórico
    - Criando table de histórico de atividade;
    - Aplicando estilização na table;
    - Colorindo coluna de status da task;

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,64 @@
+import { HistoryContainer, HistoryList, StatusTask } from './styles'
+
+export function History() {
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <StatusTask status_color="yellow">Em andamento</StatusTask>
+              </td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <StatusTask status_color="green">Concluída</StatusTask>
+              </td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <StatusTask status_color="yellow">Em andamento</StatusTask>
+              </td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <StatusTask status_color="yellow">Em andamento</StatusTask>
+              </td>
+            </tr>
+            <tr>
+              <td>Conserto de débitos técnicos</td>
+              <td>25 minutos</td>
+              <td>Há cerca de 2 meses</td>
+              <td>
+                <StatusTask status_color="red">Interrompida</StatusTask>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
+  )
+}
